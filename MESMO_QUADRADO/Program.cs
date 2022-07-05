f71cca8e7b88fac8e0dd44fc8b1dc6716fb94a8b
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-05 00:18:03 -0300

    primeiro commit

---
+++ MESMO_QUADRADO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MESMO_QUADRADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string entrada;
+
+            Console.Write("Entre com um valor: ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int n);
+
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}^2 = {Math.Pow(i,2)}.");
+                }
+            }
+        }
+    }
+}
